b8767ebd2f88ca2c711bcffbae32b33fe07ea511
gxbsst <gxbsst@gmail.com>
2013-09-29 12:24:18 +0800

    add page single

--- wp-content/themes/vj/single.php
+++ wp-content/themes/vj/single.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
 <div id="site-content" class="container home-container" role="main">
 	<div class="row-fluid page">
-		<section id="section-content" class="clearfix span9">
-			single here
+		<section id="section-content" class="clearfix span8">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<h2><?php the_title(); ?></h2>
+		<?php the_content( $more_link_text = null, $stripteaser = false )(); ?>
+
+		<?php comments_template('', true); ?>
+		
+		<?php endwhile; ?>
+
 		</section>
-		<aside class="sidebar span3 clearfix">
+		<aside class="sidebar span4 clearfix">
 			<?php get_template_part( 'sidebar'); ?>
 		</aside> 
 	</div>
